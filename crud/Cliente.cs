9a1c89db264e3a2c61a94d3e09d2717b978d7716
IFTM <IFTM@LABINF05-M17>
2026-08-12 21:10:55 -0300

    Adicionar arquivos de projeto.

---
+++ crud/Cliente.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace crud
+{
+    public class Cliente
+    {
+
+        public int Id { get; set; }
+        public string Nome { get; set; }
+
+    }
+
+}
---
+++ crud/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using crud;
+
+List<Cliente> clientes = new List<Cliente>();
+int i=0;
+
+do
+{
+    Console.WriteLine("\n1 criar cliente");
+    Console.WriteLine("2 editar cliente");
+    Console.WriteLine("3 excluir cliente");
+    Console.WriteLine("4 listar cliente");
+    Console.WriteLine("5 sair");
+    string opcao = Console.ReadLine();
+    i = int.Parse(opcao);
+
+
+    if (i == 1)
+    {
+        Cliente cliente1 = new Cliente();
+        Console.WriteLine("criando cliente");
+
+
+        Console.WriteLine("Digite o ID:");
+        cliente1.Id = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Digite o Nome:");
+        cliente1.Nome = Console.ReadLine();
+        clientes.Add(cliente1);
+
+    }
+
+    if (i==2)
+    {
+        
+    }
+
+    if (i == 4)
+    {
+        Console.WriteLine("Listando clientes:\n");
+        foreach (Cliente c in clientes)
+        {
+            Console.WriteLine($"ID: {c.Id} | Nome: {c.Nome}");
+        }
+    }
+
+
+
+}
+while (i !=5 );
